5e12284e3d3f8e027d8321c8fda3df1bf642be82
AnyiGP <angelesgp31@hotmail.com>
2023-12-09 12:01:51 -0300

    RRD

---
+++ src/Pages/Store.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link, Outlet} from "react-router-dom"
+
+export const Store = () => {
+  return (
+    <div>Store
+         <header>
+            <nav>
+                <Link to="/store" >Store</Link>
+                <Link to="/cart" >Cart</Link>
+                <Link to="/product" >Product</Link>
+            </nav>
+        </header>
+        <main>
+          <h1>Enrutado</h1>
+            <Outlet />
+        </main>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ContextFilter } from './FContext/ContextFilter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ContextFilter } from "./FContext/ContextFilter";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Cart } from "./Pages/Cart";
+import { Product } from "./Pages/Product";
+import { Store } from "./Pages/Store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<ContextFilter>
-    <App />
+    <ContextFilter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Store />}>Store</Route>
+          <Route path="/store" element={<App />}>App</Route>
+          <Route path="/cart" element={<Cart />}>Cart</Route>
+          <Route path="/product" element={<Product />}>Product</Route>
+        </Routes>
+      </BrowserRouter>
     </ContextFilter>
   </React.StrictMode>
 );
